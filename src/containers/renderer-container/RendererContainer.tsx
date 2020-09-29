b13d541c290cd9a62e35fa16c4e596273a1dc8de
GewoonRene <rhuibertsjr@gmail.com>
2020-09-29 11:50:12 +0200

    Fixed: Less types

--- src/containers/renderer-container/RendererContainer.tsx
+++ src/containers/renderer-container/RendererContainer.tsx
@@ -1,24 +1,18 @@
 import React, { PureComponent, createRef } from 'react';
+import s from './renderer.module.less';
 import * as THREE from 'three';
 
 class RendererContainer extends PureComponent
 {
     private readonly scene: THREE.Scene = new THREE.Scene();
     private readonly camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-    private readonly renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
-    private rendererRef: React.RefObject<any> = createRef();
+    private readonly renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
+    private readonly rendererRef: React.RefObject<any> = createRef();
 
     public componentDidMount(): void
     {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.rendererRef.current.appendChild(this.renderer.domElement);
-
-        var geometry = new THREE.BoxGeometry();
-        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        var cube = new THREE.Mesh( geometry, material );
-        this.scene.add( cube );
-
-        this.camera.position.z = 5;
 
         this.componentRunRenderer();
     }
@@ -31,11 +25,10 @@ class RendererContainer extends PureComponent
 
     public render(): JSX.Element
     {
-        return (
-            <div ref={this.rendererRef}>
-
-            </div>
-        )
+        return <div
+            className={s.container}
+            ref={this.rendererRef}
+        />
     }
 }
 
